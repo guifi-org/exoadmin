8bbb5447591061cc7a107c5dfcd0b51b164bfe1b
pedro-nonfree <pedro-nonfree@users.noreply.github.com>
2017-01-09 19:38:42 +0100

    update

--- identified/models.py
+++ identified/models.py
@@ -5,22 +5,27 @@ from django.contrib.auth.models import User
 
 class Identified(models.Model):
 
-    domicile = models.CharField(_("Domicile"), max_length=50)
+    # src https://docs.djangoproject.com/en/1.10/topics/auth/customizing/#extending-the-existing-user-model
+    user = models.OneToOneField(User, verbose_name=_("User"), on_delete=models.CASCADE)
+
+    address = models.CharField(_("Address"), max_length=50)
 
     # approximation; fix it to be more precise
-    identity = models.CharField(_("Identity"), max_length=8, unique=True)
+    identity = models.CharField(_("Identity"), max_length=9, unique=True)
 
     place = models.ForeignKey('location.Place', verbose_name=_("Place"), on_delete=models.CASCADE)
 
     postal_code = models.CharField(_("Postal code"), max_length=10)
 
-    # src https://docs.djangoproject.com/en/1.10/topics/auth/customizing/#extending-the-existing-user-model
-    user = models.OneToOneField(User, verbose_name=_("User"), on_delete=models.CASCADE)
-
-    # TODO usertype
     # src https://docs.djangoproject.com/en/dev/ref/models/instances/#django.db.models.Model.get_FOO_display
+    # src http://stackoverflow.com/questions/12725720/django-choices-how-to-set-default-option
     # src https://docs.djangoproject.com/en/dev/ref/forms/fields/#choicefield
     # src http://stackoverflow.com/questions/4274243/django-print-choices-value
+    USER_TYPE = (
+            ('I', 'Individual'),
+            ('O', 'Organization'),
+    )
+    user_type = models.CharField(_("Type of user"), max_length=2, choices=USER_TYPE, default='I')
 
     class Meta:
         verbose_name = _("Identified")
--- location/models.py
+++ location/models.py
@@ -22,17 +22,3 @@ class Place(models.Model):
 
     def __str__(self):
         return self.name
-
-#class Address(models.Model):
-#    city = models.ForeignKey(City, verbose_name=_("City"), on_delete=models.CASCADE)
-#    address = models.CharField(_("address"), max_length=100)
-#    postal_code = models.CharField(_("postal code"), max_length=10)
-## FIXME
-##    person = models.ForeignKey('accounting.Person', verbose_name=_("person"), blank=True, null=True, related_name='+', on_delete=models.CASCADE)
-#
-#    class Meta:
-#        verbose_name = _("Address")
-#        verbose_name_plural = _("Addresses")
-#
-#    def __str__(self):
-#        return "{} {} {}".format(self.address, self.postal_code, self.city.name)
