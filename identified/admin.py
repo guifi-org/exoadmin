1f7cbe27fc83825f76c22bef06e0123827b9b3cf
pedro-nonfree <pedro-nonfree@users.noreply.github.com>
2017-01-10 01:42:37 +0100

    improve format identified

--- identified/admin.py
+++ identified/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from . import models
 
-#@admin.register(models.Identified)
-
-admin.site.register(models.Identified)
+@admin.register(models.Identified)
+class IdentifiedAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_type')
+    list_filter = ('user_type',)
