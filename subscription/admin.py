6b12ecb68275b0b9a9214a713e5d99aca872b5b3
pedro-nonfree <pedro-nonfree@users.noreply.github.com>
2017-01-12 11:35:46 +0100

    show description on django admin list

--- subscription/admin.py
+++ subscription/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Service)
+@admin.register(models.Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
 
 @admin.register(models.Subscriber)
 class SubscriberAdmin(admin.ModelAdmin):
