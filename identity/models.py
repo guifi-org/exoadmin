be1b3e34242907612ec71e724fbf813989eeaead
pedro-nonfree <pedro-nonfree@users.noreply.github.com>
2017-01-15 03:56:25 +0100

    clean identity models

--- identity/models.py
+++ identity/models.py
@@ -7,32 +7,32 @@ class Identity(models.Model):
 
     # src https://docs.djangoproject.com/en/1.10/topics/auth/customizing/#extending-the-existing-user-model
     user = models.OneToOneField(
-            User,
-            verbose_name=_("User"),
-            on_delete=models.CASCADE
+        User,
+        verbose_name=_('User'),
+        on_delete=models.CASCADE
     )
 
     address = models.CharField(
-            _("Address"),
-            max_length=50
+        _('Address'),
+        max_length=50
     )
 
     identity = models.CharField(
-            _("ID"),
-            help_text=_("Identity Document; the number and/or text that verifies you"),
-            max_length=20,
-            unique=True,
+        _('ID'),
+        help_text=_('Identity Document; the number and/or text that verifies you'),
+        max_length=20,
+        unique=True,
     )
 
     place = models.ForeignKey(
-            'location.Place',
-            verbose_name=_("Place"),
-            on_delete=models.CASCADE
+        'location.Place',
+        verbose_name=_('Place'),
+        on_delete=models.CASCADE
     )
 
     postal_code = models.CharField(
-            _("Postal code"),
-            max_length=10
+        _('Postal code'),
+        max_length=10
     )
 
     # how to do choices
@@ -41,45 +41,45 @@ class Identity(models.Model):
     # src https://docs.djangoproject.com/en/dev/ref/forms/fields/#choicefield
     # src http://stackoverflow.com/questions/4274243/django-print-choices-value
     USER_TYPE = (
-            ('Individual', _('Individual')),
-            ('Organization', _('Organization')),
+        ('Individual', _('Individual')),
+        ('Organization', _('Organization')),
     )
     user_type = models.CharField(
-            _("Type of user"),
-            max_length=20,
-            choices=USER_TYPE,
-            default='Individual'
+         _('Type of user'),
+         max_length=20,
+         choices=USER_TYPE,
+         default='Individual'
     )
 
     # how to prepare choices for translation
     # src http://stackoverflow.com/questions/16088849/how-to-translate-dynamic-form-choices/16088999#16088999
     # [cat] formes de pagament
     PAYMENT_MEANS = (
-            # [cat] domiciliació bancària
-            ('Direct Debit', _('Direct Debit')),
-            # [cat] transferència periòdica
-            ('Periodic Transfer', _('Periodic Transfer')),
-            # [cat] metàl·lic
-            ('Cash', _('Cash')),
+        # [cat] domiciliació bancària
+        ('Direct Debit', _('Direct Debit')),
+        # [cat] transferència periòdica
+        ('Periodic Transfer', _('Periodic Transfer')),
+        # [cat] metàl·lic
+        ('Cash', _('Cash')),
     )
     payment_means = models.CharField(
-            _("Payment means"),
-            max_length=20,
-            choices=PAYMENT_MEANS,
-            default='Direct Debit'
+        _('Payment means'),
+        max_length=20,
+        choices=PAYMENT_MEANS,
+        default='Direct Debit'
     )
 
     # TODO check https://en.wikipedia.org/wiki/International_Bank_Account_Number#Structure
     iban = models.CharField(
-            ("IBAN"),
-            max_length=32,
-            blank=True,
-            help_text=_("International Bank Account Number")
+        ('IBAN'),
+        max_length=32,
+        blank=True,
+        help_text=_('International Bank Account Number')
     )
 
     class Meta:
-        verbose_name = _("Identity")
-        verbose_name_plural = _("Identities")
+        verbose_name = _('Identity')
+        verbose_name_plural = _('Identities')
 
     def __str__(self):
         return self.user.username
