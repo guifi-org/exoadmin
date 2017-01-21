40dc4205123030fad818bbb50cae81fd8de5d721
pedro-nonfree <pedro-nonfree@users.noreply.github.com>
2017-01-21 21:47:11 +0100

    StackedInline for subscription case

--- subscription/admin.py
+++ subscription/admin.py
@@ -10,7 +10,7 @@ from netinterface.models import Network_interface
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
 
-class NetinterfaceInline(admin.TabularInline):
+class NetinterfaceInline(admin.StackedInline):
     model = Network_interface
     extra = 0
 
